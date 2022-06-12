974b1accc6bc3f29c440f637dc04a1cdc3ec098c
BloodLad7 <jayikedis@gmail.com>
2022-06-12 20:55:39 +0100

    init

---
+++ dict.py
@@ -0,0 +1,27 @@
+from unicodedata import name
+
+
+highbloodpressure = { 'defintion': 'High blood pressure (hypertension) is a common condition in which the long-term force of the blood against your \nartery walls is high enough that it may eventually cause health problems, such as heart disease.' ,'treatment': 'Decrease the salt in your diet. Aim to limit sodium to less than 2,300 milligrams (mg) a day or less.', 'food': 'Salmon and other fatty fish'}
+
+diarrhea = { 'defintion': '' ,'treatment': 'Drink plenty of liquids, including water, broths and juices. Avoid caffeine and alcohol.',}          
+
+coldsandFlu = { 'defintion': '' ,'treatment': 'Stay hydrated. Water, juice, clear broth or warm lemon water with honey helps loosen congestion and prevents dehydration.'}
+
+stomachAches = { 'defintion': '' ,'treatment': 'The body needs water to digest and absorb nutrients from foods and beverages efficiently. \nBeing dehydrated makes digestion more difficult and less effective, which increases the likelihood of an upset stomach.'}
+
+stroke = { 'defintion': '' ,'treatment': 'An IV injection of recombinant tissue plasminogen activator (TPA) — also called alteplase (Activase) or tenecteplase  \n(TNKase) — is the gold standard cure for ischemic stroke. An injection of TPA is usually given through a vein in the arm within the first three hours.'}
+
+allergies = { 'defintion': 'Allergies are an immune response triggered by allergens, an ordinarily harmful substance.' ,'treatment': ''}
+
+
+
+
+
+
+
+
+
+print(highbloodpressure['treatjment'])
+
+
+

5f0a9e79b0bbf98788c9a96575cb0ecd18f79d20
BloodLad7 <jayikedis@gmail.com>
2022-06-13 22:35:41 +0100

    name change

--- dict.py
+++ dict.py
@@ -7,21 +7,21 @@ diarrhea = { 'defintion': '' ,'treatment': 'Drink plenty of liquids, including w
 
 coldsandFlu = { 'defintion': 'Influenza (flu) and the common cold are both contagious respiratory illnesses, but they are caused by different viruses. Flu is caused by influenza viruses \nonly, whereas the common cold can be caused by a number of different viruses, including rhinoviruses, parainfluenza, and seasonal coronaviruses.' ,'treatment': 'Stay hydrated. Water, juice, clear broth or warm lemon water with honey helps loosen congestion and prevents dehydration.'}
 
-stomachAches = { 'defintion': '' ,'treatment': 'The body needs water to digest and absorb nutrients from foods and beverages efficiently. \nBeing dehydrated makes digestion more difficult and less effective, which increases the likelihood of an upset stomach.'}
+stomachAches = { 'defintion': 'It is natural to have gas in your digestive tract, and to pass gas regularly. In fact, everyone passes gas at least 12 times a day, which is a combination \nof oxygen, hydrogen, nitrogen, carbon dioxide, and methane. The unpleasant odor of flatus is cause by gases (e.g. hydrogen sulfide) \nproduced by the bacteria in the large intestine. Even though it is natural to have gas in your digestive tract, excessive gas can cause intense, temporary pain.' ,'treatment': 'The body needs water to digest and absorb nutrients from foods and beverages efficiently. \nBeing dehydrated makes digestion more difficult and less effective, which increases the likelihood of an upset stomach.'}
 
 stroke = { 'defintion': '' ,'treatment': 'An IV injection of recombinant tissue plasminogen activator (TPA) — also called alteplase (Activase) or tenecteplase  \n(TNKase) — is the gold standard cure for ischemic stroke. An injection of TPA is usually given through a vein in the arm within the first three hours.'}
 
 allergies = { 'defintion': 'Allergies are an immune response triggered by allergens, an ordinarily harmful substance.' ,'treatment': 'The easiest and most effective way to treat allergies is to get rid of or avoid the cause. Where unavoidable, some lifestyle changes can reduce \nyour allergy symptoms. For example, if you are allergic to dust mites, make an effort to keep your room clean and free of dust \nby frequent vacuuming, dusting, and washing of bedding.'}
 
-Conjunctivitis = { 'defintion': '' ,'treatment': '' ,'symptoms': 'These symptoms may last a few hours to several weeks: redness, itching, tearing, burning sensation, pus-like discharge and/or crusting of the eyelids. \nBecause conjunctivitis causes inflammation of the small blood vessels in the conjunctiva to become more prominent, the whites of your \neyes will appear pink or red.'}
+conjunctivitis = { 'defintion': 'Pink eye (conjunctivitis) is the inflammation or infection of the transparent membrane that lines your eyelid and eyeball. It is characterized \nby redness and a gritty sensation in your eye, along with itching. Often a discharge forms a crust on your eyelashes during the night.' ,'treatment': '' ,'symptoms': 'These symptoms may last a few hours to several weeks: redness, itching, tearing, burning sensation, pus-like discharge and/or crusting of the eyelids. \nBecause conjunctivitis causes inflammation of the small blood vessels in the conjunctiva to become more prominent, the whites of your \neyes will appear pink or red.'}
+
+asthma = {'defintion': 'Asthma is a chronic lung disease characterized by episodes of airway narrowing and obstruction, causing wheezing, coughing, chest tightness and shortness of breath.'}
 
 
 
 
 
-
-
-print(stroke['treatment'])
+print(Conjunctivitis['defintion'])
 
 
 
